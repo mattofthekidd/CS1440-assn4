883f76d5c0c1d2a517cb928a251a67460a7606fb
Matthew Kidd <mattofthekidd@gmail.com>
2017-02-26 10:06:45 -0700

    going to create branch to make NumberList.cpp/hpp a reality

--- NumberList.cpp
+++ NumberList.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "NumberList.hpp"
+
+void NumberList::createRandomNumberList() {
+    for (auto i = 0; i < m_maxNum - 1; i++) {
+        m_numberList.push_back(i + 1);
+    }
+    std::random_shuffle(m_numberList.begin(), m_numberList.end());
+}
--- NumberList.hpp
+++ NumberList.hpp
@@ -8,6 +8,7 @@
 
 class NumberList {
 public:
+
 private:
 };
 
